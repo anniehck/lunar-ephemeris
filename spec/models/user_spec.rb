9d9072164272014a762e484ea5d043d7d8368f66
anniehck <anniehckim@gmail.com>
2016-10-15 21:34:49 -0400

    edited user test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,8 @@
+require 'spec_helper'
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_valid(:first_name).when('Bob', 'Amanda') }
+  it { should have_valid(:last_name).when('Smith', 'Doe') }
+  it { should have_valid(:username).when('userName', 'hello_world') }
 end
