55a803bb5857a24597adc1adb452c2266a237a5f
anniehck <anniehckim@gmail.com>
2016-10-16 12:01:19 -0400

    added User validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates :username, length: { minimum: 4, maximum: 16 }
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, format: { with: /.+@.+\..+/ }
+  validates :password, length: { minimum: 6, maximum: 16 }
+
   def admin?
     role == 'admin'
   end
