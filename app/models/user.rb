391ce596635c606fd795951382fa6e73e1f90aae
anniehck <anniehckim@gmail.com>
2016-10-16 12:41:06 -0400

    Feature tests for user registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates :username, length: { minimum: 4, maximum: 16 }
+  validates :username, presence: true, uniqueness: true, length: { minimum: 4, maximum: 16 }
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, format: { with: /.+@.+\..+/ }
