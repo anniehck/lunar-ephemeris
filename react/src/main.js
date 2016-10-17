95cde2f6c22e6ffb79f21e08009ee194ac19045c
anniehck <anniehckim@gmail.com>
2016-10-17 14:40:09 -0400

    WIP with clean urls, issue with refresh

--- react/src/main.js
+++ react/src/main.js
@@ -1,14 +1,14 @@
 import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, hashHistory, IndexRoute } from 'react-router';
+import { Router, Route, browserHistory, IndexRoute } from 'react-router';
 import App from './components/App';
 import Home from './components/Home';
 import About from './components/About';
 
 $(function() {
   ReactDOM.render(
-    <Router history={hashHistory}>
+    <Router history={browserHistory}>
       <Route path="/" component={App}>
         <IndexRoute component={Home} />
 
