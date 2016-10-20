0b430f4c74093f1aa57abc6cfcce1de294b48c8d
anniehck <anniehckim@gmail.com>
2016-10-19 20:17:39 -0400

    passing User model tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,9 @@ class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true, length: { minimum: 4, maximum: 16 }
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, format: { with: /.+@.+\..+/ }
+  validates :email, format: { with: /.+@.+\..+/ }, uniqueness: true
   validates :password, length: { minimum: 6, maximum: 16 }
+  validates :password_confirmation, presence: true
 
   def admin?
     role == 'admin'
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,8 +2,6 @@ require 'spec_helper'
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let!(:user) { User.new(username: 'astroguy', first_name: 'Stephen', last_name: 'Hawking', email: 'astrophys@gmail.com', password: 'password', password_confirmation: 'password' )}
-
   it { should validate_presence_of(:username) }
   it { should validate_presence_of(:first_name) }
   it { should validate_presence_of(:last_name) }
@@ -16,12 +14,10 @@ RSpec.describe User, type: :model do
   it { should have_valid(:username).when('userName', 'hello_world') }
   it { should have_valid(:email).when('username@gmail.com', 'my_email@hotmail.com') }
   it { should_not have_valid(:email).when('username@gmail', '@aol.com') }
-  it { should validate_uniqueness_of(:email) }
   it { should validate_length_of(:username).is_at_least(4) }
   it { should validate_length_of(:username).is_at_most(16) }
   it { should_not have_valid(:username).when('hi', 'make_an_applepie_from_scratch') }
-  it { should validate_uniqueness_of(:username) }
   it { should validate_length_of(:password).is_at_least(6) }
   it { should validate_length_of(:password).is_at_most(16) }
-  it { should_not have_valid(:password).when('pass', '') }
+  it { should_not have_valid(:password).when('pass', 'word') }
 end
