41c8527d503040871ea06aabbc3eb7a14ebf6c4f
anniehck <anniehckim@gmail.com>
2016-10-21 23:33:02 -0400

    passing 44 tests!

--- spec/features/user_edits_account_spec.rb
+++ spec/features/user_edits_account_spec.rb
@@ -2,20 +2,17 @@ require 'rails_helper'
 
 feature 'User edits account' do
   let!(:user) { FactoryGirl.create(:user) }
-
-  scenario 'authenticated user can update account' do
+  before(:each) do
     user_signs_in(user)
     click_link 'face'
     click_link 'Update Account'
+  end
 
+  scenario 'authenticated user can update account' do
     expect(page).to have_content 'Edit User'
   end
 
   scenario 'user provides current password to successfully update account' do
-    user_signs_in(user)
-    click_link 'face'
-    click_link 'Update Account'
-
     fill_in 'Email', with: user.email
     fill_in 'Password', with: 'password2'
     fill_in 'Password confirmation', with: 'password2'
@@ -24,4 +21,27 @@ feature 'User edits account' do
 
     expect(page).to have_content 'Your account has been updated successfully'
   end
+
+  scenario 'user does not fill out all required fields' do
+    fill_in 'Email', with: ''
+    fill_in 'Password', with: 'password3'
+    fill_in 'Password confirmation', with: 'password3'
+    fill_in 'Current password', with: user.password
+    click_button 'Update'
+
+    expect(page).to have_content '2 errors prohibited this user from being saved'
+    expect(page).to have_content 'Email can\'t be blank'
+    expect(page).to have_content 'Email is invalid'
+  end
+
+  scenario 'password does not match password confirmation' do
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: 'password2'
+    fill_in 'Password confirmation', with: 'password'
+    fill_in 'Current password', with: user.password
+    click_button 'Update'
+
+    expect(page).to have_content 'Password confirmation doesn\'t match Password'
+  end
+  scenario 'does not fill out current password correctly'
 end
