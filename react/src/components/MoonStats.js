7868e3bbe742c9c83325c68c6fbbfa2ef22c9b9a
anniehck <anniehckim@gmail.com>
2016-10-22 21:18:08 -0400

    set default values for lat,lon & changed date/time logic incase null

--- react/src/components/MoonStats.js
+++ react/src/components/MoonStats.js
@@ -4,9 +4,10 @@ class MoonStats extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      city: '',
       moon: [],
-      name: '',
+      city: '',
+      date: '',
+      moonPhase: '',
       illum: '',
       lat: '',
       lon: ''
@@ -20,16 +21,21 @@ class MoonStats extends Component {
     })
     .done(data => {
       if (data.moon.length !== 0) {
-        this.setState({ city: data.city, moon: data.moon, name: data.moon.phase.name, illum: data.moon.phase.illum })
+        this.setState({ city: data.city, date: data.date, moon: data.moon, moonPhase: data.moon.phase.name, illum: data.moon.phase.illum })
       }
     });
   }
 
   render() {
     let moonStats = this.state.moon;
-    let date = new Date(moonStats.riseISO).toString().substr(0, 15);
-    let riseTime = new Date(moonStats.riseISO).toString().substr(16).slice(0, 5);
-    let setTime = new Date(moonStats.setISO).toString().substr(16).slice(0, 5);
+    let date = new Date(this.state.date).toString().substr(0, 15);
+    let riseTime = '?';
+    let setTime = '?';
+    if (moonStats.riseISO !== null) {
+      riseTime = new Date(moonStats.riseISO).toString().substr(16).slice(0, 5);
+    } else if (moonStats.setISO !== null) {
+      setTime = new Date(moonStats.setISO).toString().substr(16).slice(0, 5);
+    }
 
     return(
       <div className="moon-stats content">
@@ -37,7 +43,7 @@ class MoonStats extends Component {
         <h3>The Moon in {this.state.city} Tonight</h3>
         <p>{date}</p>
         <p>The moon rises at {riseTime} and sets at {setTime}</p>
-        <p className="phase">{this.state.name}</p>
+        <p className="phase">{this.state.moonPhase}</p>
         <p>illumination at <span className="blue">{this.state.illum}</span></p>
       </div>
     )
